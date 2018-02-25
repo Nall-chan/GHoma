77448458d42f9e20029e187a132793703d80d4de
Nall-chan <micha@nall-chan.net>
2018-02-25 11:00:14 +0000

    Apply fixes from StyleCI

--- GHPlug/module.php
+++ GHPlug/module.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 /**
  * @addtogroup ghoma
  * @{
@@ -29,7 +29,6 @@ require_once __DIR__ . '/../libs/GHomaTraits.php';  // diverse Klassen
  */
 class GHomaPlug extends IPSModule
 {
-
     use VariableHelper,
         DebugHelper,
         InstanceStatus,
@@ -37,6 +36,7 @@ class GHomaPlug extends IPSModule
         VariableProfile {
         InstanceStatus::MessageSink as IOMessageSink; // MessageSink gibt es sowohl hier in der Klasse, als auch im Trait InstanceStatus. Hier wird für die Methode im Trait ein Alias benannt.
     }
+
     /**
      * Interne Funktion des SDK.
      */
@@ -225,6 +225,7 @@ class GHomaPlug extends IPSModule
     }
 
     //################# PUBLIC
+
     /**
      * IPS-Instanz Funktion GHOMA_SendSwitch.
      * Schaltet den Controller ein oder aus.
@@ -269,6 +270,7 @@ class GHomaPlug extends IPSModule
     }
 
     //################# ActionHandler
+
     /**
      * Interne Funktion des SDK.
      */
@@ -285,6 +287,7 @@ class GHomaPlug extends IPSModule
     }
 
     //################# PRIVATE
+
     /**
      * Sendet die Initialisierung an den Controller und prüft die Rückmeldung.
      *
@@ -413,6 +416,7 @@ class GHomaPlug extends IPSModule
     }
 
     //################# DATAPOINTS
+
     /**
      * Interne Funktion des SDK.
      */
@@ -494,7 +498,6 @@ class GHomaPlug extends IPSModule
                             'Buffer'     => utf8_encode($Message->toFrame())])
         );
     }
-
 }
 
 /* @} */
